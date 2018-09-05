a2906a4c408cdf82093e712ccc9256b995a9f875
Sam Marshall <samuel.r.marshall@gmail.com>
2018-09-04 23:52:32 -0400

    developing party show

--- app/assets/javascripts/parties.js
+++ app/assets/javascripts/parties.js
@@ -1,9 +1,25 @@
 var name = ""
 var id = null
 var test = {}
+var attendees = []
+var dishes = []
 $.getJSON(window.location.href,function(data){
+  var i = 0;
   test = data
-  $("#party-name").text(data.name)
+  id = data.id
+  name = data.name
+  attendees = data.attendees
+  dishes = data.dishes
+  // host = $.getJSON(`/users/${data.host_id}`)
+  $("#party-name").text(name)
+  $("#edit-party").html(`<a href="./${id}/edit">Edit ${name}</a>`)
+  for (i = 0; i < attendees.length; i++){
+    $("#attendees-list").append(`<li>${attendees[i].name}</li>`)
+  }
+  for (i = 0; i < dishes.length; i++){
+    $("#dishes-list").append(`<li>${dishes[i].id}</li>`)
+    // needs food lookup
+  }
 })
 // $(function(){
 //   $.get("/parties/"+id+".json", function(data){
--- app/serializers/party_serializer.rb
+++ app/serializers/party_serializer.rb
@@ -1,9 +1,5 @@
 class PartySerializer < ActiveModel::Serializer
-  has_many :users
-  has_many :drinks
-  belongs_to :host, class_name: 'User'
-  has_and_belongs_to_many :attendees, class_name: 'User'
-  has_many :dishes
-  has_many :foods, through: :dishes
-  attributes :id, :name, :address, :host_id, :time
+  attributes :id, :name, :address, :host_id, :time, :attendees, :dishes
+
+
 end
